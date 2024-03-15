a5e9241f1351d50cd9c4664221de59784def9514
telegeyN <ntelegey@gmail.com>
2024-03-16 00:00:45 +0600

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Telegey Nurbekova on 16/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
